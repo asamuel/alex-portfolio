b521926d7dd2f7a13e35915db530fe3bc71b66a6
asamuel <samuelxlm0@gmail.com>
2026-07-10 16:08:05 -0600

    fix: delete icon prop in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,23 +24,6 @@ export const metadata: Metadata = {
   authors: [{ name: profile.seo.author }],
   creator: profile.seo.creator,
   keywords: profile.seo.keywords,
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
   openGraph: {
     title: profile.seo.title,
     description: profile.seo.description,
